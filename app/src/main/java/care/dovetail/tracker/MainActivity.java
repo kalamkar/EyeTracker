05b50a35a3253692bfa3f15063669d97e070aa46
Abhijit Kalamkar <abhi@Abhijits-MacBook-Air-2.local>
2017-02-15 14:43:19 -0800

    Changed settings dialog to activity to accomodate more widgets

--- app/src/main/java/care/dovetail/tracker/MainActivity.java
+++ app/src/main/java/care/dovetail/tracker/MainActivity.java
@@ -2,6 +2,7 @@ package care.dovetail.tracker;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.SensorManager;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
@@ -23,7 +24,7 @@ import care.dovetail.tracker.processing.Feature;
 import care.dovetail.tracker.processing.SignalProcessor;
 import care.dovetail.tracker.ui.ChartFragment;
 import care.dovetail.tracker.ui.GridView;
-import care.dovetail.tracker.ui.SettingsDialog;
+import care.dovetail.tracker.ui.SettingsActivity;
 
 public class MainActivity extends Activity implements BluetoothDeviceListener,
         SignalProcessor.FeatureObserver, AccelerationProcessor.ShakingObserver {
@@ -54,20 +55,13 @@ public class MainActivity extends Activity implements BluetoothDeviceListener,
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-        findViewById(R.id.binocular).setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 
         findViewById(R.id.settings).setOnLongClickListener(
                 new View.OnLongClickListener() {
                     @Override
                     public boolean onLongClick(View view) {
                         stopBluetooth();
-                        new SettingsDialog().show(getFragmentManager(), null);
+                        startActivity(new Intent(MainActivity.this, SettingsActivity.class));
                         return true;
                     }
                 });
@@ -92,6 +86,7 @@ public class MainActivity extends Activity implements BluetoothDeviceListener,
     @Override
     protected void onStart() {
         super.onStart();
+        hideBars();
         startBluetooth();
         showDualView(false);
         Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
@@ -289,5 +284,15 @@ public class MainActivity extends Activity implements BluetoothDeviceListener,
         }
         moleChangeCount = 0;
         moleSector = null;
+    }
+
+    private void hideBars() {
+        findViewById(R.id.binocular).setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
     }
 }
--- app/src/main/java/care/dovetail/tracker/ui/SettingsActivity.java
+++ app/src/main/java/care/dovetail/tracker/ui/SettingsActivity.java
@@ -1,22 +1,13 @@
 package care.dovetail.tracker.ui;
 
-import android.app.Dialog;
-import android.app.DialogFragment;
-import android.content.Context;
-import android.content.DialogInterface;
+import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.Window;
 import android.widget.CompoundButton;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
-import care.dovetail.tracker.MainActivity;
 import care.dovetail.tracker.R;
 import care.dovetail.tracker.Settings;
 
@@ -24,10 +15,10 @@ import care.dovetail.tracker.Settings;
  * Created by abhi on 2/6/17.
  */
 
-public class SettingsDialog extends DialogFragment {
-    private final static String TAG = "SettingsDialog";
+public class SettingsActivity extends Activity {
+    private final static String TAG = "SettingsActivity";
 
-    private Settings settings;
+    private final Settings settings = new Settings(this);
 
     private ToggleButton dayDream;
     private ToggleButton showChart;
@@ -42,44 +33,23 @@ public class SettingsDialog extends DialogFragment {
     private TextView vToHValue;
 
     @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        settings = new Settings(context);
-    }
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
 
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        super.onCreateView(inflater, container, savedInstanceState);
-        return inflater.inflate(R.layout.fragment_settings, container);
-    }
+        SharedPreferences prefs = getSharedPreferences(getPackageName(), 0);
 
-    @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
-        Dialog dialog = super.onCreateDialog(savedInstanceState);
-        dialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
-        return dialog;
-    }
-
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        SharedPreferences prefs =
-                getActivity().getSharedPreferences(getActivity().getPackageName(), 0);
-
-        dayDream = (ToggleButton) view.findViewById(R.id.dayDream);
-        showChart = (ToggleButton) view.findViewById(R.id.showChart);
-        showBlinks = (ToggleButton) view.findViewById(R.id.showBlinks);
-        whackAMole = (ToggleButton) view.findViewById(R.id.whackAMole);
-        showAccel = (ToggleButton) view.findViewById(R.id.showAccel);
-        numSteps = (SeekBar) view.findViewById(R.id.num_steps);
-        numStepsValue = (TextView)  view.findViewById(R.id.num_steps_value);
-        blinkToGaze = (SeekBar) view.findViewById(R.id.blink_to_gaze);
-        blinkToGazeValue = (TextView)  view.findViewById(R.id.blink_to_gaze_value);
-        verticalToHorizontal = (SeekBar) view.findViewById(R.id.v_to_h);
-        vToHValue = (TextView)  view.findViewById(R.id.v_to_h_value);
+        dayDream = (ToggleButton) findViewById(R.id.dayDream);
+        showChart = (ToggleButton) findViewById(R.id.showChart);
+        showBlinks = (ToggleButton) findViewById(R.id.showBlinks);
+        whackAMole = (ToggleButton) findViewById(R.id.whackAMole);
+        showAccel = (ToggleButton) findViewById(R.id.showAccel);
+        numSteps = (SeekBar) findViewById(R.id.num_steps);
+        numStepsValue = (TextView)  findViewById(R.id.num_steps_value);
+        blinkToGaze = (SeekBar) findViewById(R.id.blink_to_gaze);
+        blinkToGazeValue = (TextView)  findViewById(R.id.blink_to_gaze_value);
+        verticalToHorizontal = (SeekBar) findViewById(R.id.v_to_h);
+        vToHValue = (TextView)  findViewById(R.id.v_to_h_value);
 
         dayDream.setChecked(settings.isDayDream());
         showChart.setChecked(settings.shouldShowChart());
@@ -160,18 +130,5 @@ public class SettingsDialog extends DialogFragment {
             @Override public void onStartTrackingTouch(SeekBar seekBar) {}
             @Override public void onStopTrackingTouch(SeekBar seekBar) {}
         });
-    }
-
-    @Override
-    public void onDismiss(DialogInterface dialog) {
-        ((MainActivity) getActivity()).startBluetooth();
-        getActivity().findViewById(R.id.binocular).setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-        super.onDismiss(dialog);
     }
 }
