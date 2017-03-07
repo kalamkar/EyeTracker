60b01cf3b2c246ecd28d8e49e7fdb0de280963d2
Abhijit Kalamkar <abhi@Abhijits-MacBook-Air-2.local>
2017-03-07 12:24:46 -0800

    Removed unnecessary median values from the graph

--- app/src/main/java/care/dovetail/tracker/ui/ChartFragment.java
+++ app/src/main/java/care/dovetail/tracker/ui/ChartFragment.java
@@ -17,9 +17,7 @@ import care.dovetail.tracker.ui.ChartView.Chart;
 public class ChartFragment extends Fragment {
     private static final String TAG = "ChartFragment";
 
-    private int lightBlue;
     private int darkBlue;
-    private int lightGreen;
     private int darkGreen;
     private int lightRed;
     private int orange;
@@ -30,8 +28,6 @@ public class ChartFragment extends Fragment {
     private Chart channel1;
     private Chart channel2;
     private Chart channel3;
-    private Chart median1;
-    private Chart median2;
     private Chart feature1;
     private Chart feature2;
 
@@ -39,9 +35,7 @@ public class ChartFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         darkBlue = getResources().getColor(android.R.color.holo_blue_dark);
-        lightBlue = getResources().getColor(android.R.color.holo_blue_light);
         darkGreen = getResources().getColor(android.R.color.holo_green_dark);
-        lightGreen = getResources().getColor(android.R.color.holo_green_light);
         lightRed = getResources().getColor(android.R.color.holo_red_light);
         orange = getResources().getColor(android.R.color.holo_orange_dark);
         purple = getResources().getColor(android.R.color.holo_purple);
@@ -62,12 +56,6 @@ public class ChartFragment extends Fragment {
         channel3 = chartView.makeLineChart(lightRed, 1);
         channel3.setXRange(0, Config.GRAPH_LENGTH);
 
-        median1 = chartView.makeLineChart(lightBlue, 2);
-        median1.setXRange(0, Config.GRAPH_LENGTH);
-
-        median2 = chartView.makeLineChart(lightGreen, 2);
-        median2.setXRange(0, Config.GRAPH_LENGTH);
-
         feature1 = chartView.makePointsChart(orange, 5);
         feature1.setXRange(0, Config.GRAPH_LENGTH);
 
@@ -86,13 +74,6 @@ public class ChartFragment extends Fragment {
         }
         channel1.setYRange(range.first, range.second);
         channel1.setData(points);
-
-        int median = (range.second - range.first) / 2 + range.first;
-        List<Pair<Integer, Integer>> median1Points = new ArrayList<>(2);
-        median1Points.add(Pair.create(0, median));
-        median1Points.add(Pair.create(Config.GRAPH_LENGTH - 1, median));
-        this.median1.setYRange(range.first, range.second);
-        this.median1.setData(median1Points);
     }
 
     public void updateChannel2(int data[], Pair<Integer,Integer> range) {
@@ -102,13 +83,6 @@ public class ChartFragment extends Fragment {
         }
         channel2.setYRange(range.first, range.second);
         channel2.setData(points);
-
-        int median = (range.second - range.first) / 2 + range.first;
-        List<Pair<Integer, Integer>> median2Points = new ArrayList<>(2);
-        median2Points.add(Pair.create(0, median));
-        median2Points.add(Pair.create(Config.GRAPH_LENGTH - 1, median));
-        this.median2.setYRange(range.first, range.second);
-        this.median2.setData(median2Points);
     }
 
     public void updateChannel3(int data[], Pair<Integer,Integer> range) {
