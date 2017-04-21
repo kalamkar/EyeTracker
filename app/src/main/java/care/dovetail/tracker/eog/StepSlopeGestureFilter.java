298e946d9d076062f91a950aabedd9433d0a57e1
Abhijit Kalamkar <abhi@Abhijits-MacBook-Air-2.local>
2017-04-21 11:50:47 -0700

    Added a slope gesture filter specifically for step / value change based input

---
+++ app/src/main/java/care/dovetail/tracker/eog/StepSlopeGestureFilter.java
@@ -0,0 +1,67 @@
+package care.dovetail.tracker.eog;
+
+import org.apache.commons.math3.stat.descriptive.moment.StandardDeviation;
+
+import care.dovetail.tracker.Stats;
+
+/**
+ * Created by abhi on 4/20/17.
+ */
+
+public class StepSlopeGestureFilter implements Filter {
+
+    private final int window[];
+    private final double thresholdWindow[];
+    private final float thresholdMultiplier;
+    private final int minThreshold;
+
+    private int threshold = 0;
+
+    private int countSinceUpdate = 0;
+
+    public StepSlopeGestureFilter(int windowSize, int thresholdWindowSize, float thresholdMultiplier,
+                                  int minThreshold) {
+        window = new int[windowSize];
+        thresholdWindow = new double[thresholdWindowSize];
+        this.thresholdMultiplier = thresholdMultiplier;
+        this.minThreshold = minThreshold;
+    }
+
+    @Override
+    public int filter(int value) {
+        System.arraycopy(window, 1, window, 0, window.length - 1);
+        window[window.length - 1] = value;
+
+        float slope = calculateStepSlope(window);
+
+        System.arraycopy(thresholdWindow, 1, thresholdWindow, 0, thresholdWindow.length - 1);
+        thresholdWindow[thresholdWindow.length - 1] = slope;
+
+        if (countSinceUpdate == thresholdWindow.length) {
+            double stddev = new StandardDeviation().evaluate(thresholdWindow);
+            threshold = Math.max(minThreshold, (int) (stddev * thresholdMultiplier));
+            countSinceUpdate = 0;
+        } else {
+            countSinceUpdate++;
+        }
+
+        return Math.abs(slope) > Math.abs(threshold) ? Math.round(slope) : 0;
+    }
+
+    @Override
+    public void removeSpike(int size) {
+        // RawBlinkDetector.removeSpike(thresholdWindow, size);
+    }
+
+    public int threshold() {
+        return threshold;
+    }
+
+    private static float calculateStepSlope(int window[]) {
+        Stats stats = new Stats(window);
+        int amplitude = Math.abs(stats.max - stats.min);
+        int sign = stats.maxIndex - stats.minIndex;
+        sign = sign / (sign != 0 ? Math.abs(sign) : 1);
+        return sign * amplitude;
+    }
+}
